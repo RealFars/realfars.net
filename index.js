1496bfa94659ace2a4c4ed1c99f599fea1137bd9
Fabian Grote <lassos_rumbas_0w@icloud.com>
2024-08-06 01:04:12 +0200

    added a counter if clicked on rat

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //:^3
+
+let clickCount = 0;
+
+document.getElementById("RatImage").addEventListener("click", function() {
+    clickCount++;
+
+    document.getElementById("header").textContent = "Counter:";
+        document.getElementById("header2").textContent = clickCount.toString()
+});
