06b708262aabde60ee1688ddd7d7096834e2eff9
Fabian Grote <lassos_rumbas_0w@icloud.com>
2025-02-12 20:38:12 +0100

    added the time thingy

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-//:^3
+//:^
+const date = new Date();
 
-let clickCount = 0;
-
-function imageClickCounter(){
-    clickCount++;
-    console.log("Counter: " + clickCount.toString());
+const weekdays={ 
+    "0":"Sunday", 
+    "1":"Monday", 
+    "2":"Tuesday",
+    "3":"Wednesday",
+    "4":"Thursday",
+    "5":"Friday",
+    "6":"Saturday"
 };
+
+window.setTimeout(upateDateStuff, 100);
+
+function upateDateStuff(){
+    var dateString = weekdays[date.getDay()] + ", " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    document.getElementById("dateDisplay").innerHTML = dateString;
+
+    var timeString = date.getHours() + ":" + date.getMinutes()
+    document.getElementById("timeDisplay").innerHTML = timeString;
+}
